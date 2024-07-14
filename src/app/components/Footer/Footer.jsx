1a045bb5da09151803140ef6c5c4ffc7d170a7cc
Dev_Snake <dangvanhaufpt2019@gmail.com>
2024-07-14 09:30:35 +0700

    feat(photos): add a new effect

---
+++ src/app/components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+	return (
+		<div className="p-4">
+			<h1 className="">Footer</h1>
+		</div>
+	);
+}
--- src/app/components/photos/Photos.tsx
+++ src/app/components/photos/Photos.tsx
@@ -1,11 +1,14 @@
 'use client';
 import { GlareCard } from '../ui/glare-card';
 import { Image } from '@nextui-org/react';
+import { BackgroundBeams } from '../ui/background-beams';
 export default function Photos() {
 	return (
 		<div className="w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
-			<h1 className="font-ProtestGuerrilla uppercase text-4xl tracking-[1.8px]">Photos</h1>
-			<div className="max-w-8xl grid grid-cols-4 gap-x-20 gap-y-5 mt-10 ">
+			<h1 className="font-ProtestGuerrilla uppercase text-2xl sm:text-4xl tracking-[1.8px]">
+				cr7 in clubs and national team
+			</h1>
+			<div className="max-w-8xl grid sm:grid-cols-4 gap-x-16 gap-y-5 mt-10 relative z-50">
 				<GlareCard>
 					<Image src="/images/18cr7.jpg" alt="CR7" isBlurred />
 				</GlareCard>
@@ -31,6 +34,7 @@ export default function Photos() {
 					<Image src="/images/Slider_CR7_6.jpg" alt="CR7" isBlurred />
 				</GlareCard>
 			</div>
+			<BackgroundBeams />
 		</div>
 	);
 }
